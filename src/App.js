6c64cb19a9b39b67481ae7cb01a028194d030490
Konrad Tusiński <konrad.tusinski@o2.pl>
2024-02-06 00:40:52 +0100

    Create form

--- src/App.js
+++ src/App.js
@@ -7,19 +7,23 @@ function App() {
 
   const onInputChange = ({target}) => setNewTask(target.value)
 
+  const onFormSubmit = (event) => {
+    console.log("Wciśnięto przycisk");
+    event.preventDefault()
+  }
 
   return (
-    <div>
+    <form onSubmit={onFormSubmit}>
      
         <h1>Hello</h1>
-     
       
+   
         <h2>Lista Zadań</h2>
         <input value={newTask} onChange={onInputChange} />
-        <button >Dodaj</button>
-      
+        <button>Dodaj</button>
+     
 
-    </div>
+    </form>
 
   );
 }
